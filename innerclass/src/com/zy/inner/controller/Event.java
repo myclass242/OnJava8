e5035ccb4d701f11f047f2b0935a25845554b89b
yingzhang <1037236503@qq.com>
2019-12-28 08:41:57 +0800

    innerclass

---
+++ innerclass/src/com/zy/inner/controller/Event.java
@@ -0,0 +1,19 @@
+package com.zy.inner.controller;
+
+import java.time.*;
+
+public abstract class Event {
+    private Instant eventTime;
+    protected final Duration delayTime;
+    public Event(long millisecondDelay) {
+        delayTime = Duration.ofMillis(millisecondDelay);
+        start();
+    }
+    public void start() {
+        eventTime = Instant.now().plus(delayTime);
+    }
+    public boolean ready() {
+        Instant.now().isAfter(eventTime);
+    }
+    public abstract void action();
+}
